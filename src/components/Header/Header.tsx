ca08447be4c03f24e083bba8f04813e605498b1f
agashvaratharasa <akash@vitalmasks.lk>
2024-09-20 21:50:05 +0530

    hero 1 completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../Assets/Logo/logo.png";
 import Image from "next/image";
-
 import { navLinks } from "@/constants/constants";
 import Link from "next/link";
 
 const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen); // Toggle the state correctly
+    console.log("clicked", isOpen);
+  };
+
   return (
     <div className="flex justify-center">
-      <header className="w-full fixed bg-transparent flex items-center justify-between py-4 px-8 ">
+      {/* Desktop Header */}
+      <header className="w-full md:fixed md:flex hidden bg-transparent flex items-center justify-between py-4 px-8">
         <div className="flex items-center">
           <Image src={logo} width={200} height={80} alt="Logo" />
         </div>
 
-        <nav className="flex items-center space-x-8 ">
+        <nav className="flex items-center space-x-8">
           {navLinks.map((link, index) => (
             <Link
               key={index}
@@ -26,11 +33,61 @@ const Header: React.FC = () => {
         </nav>
 
         <div>
-          <button className="text-[#FFFFFF] leading-[27.2px]  text-[16px] py-[10px] px-[35px] bg-[#603EE4] font-semibold rounded-[8px] ">
+          <button className="text-[#FFFFFF] leading-[27.2px] text-[16px] py-[10px] px-[35px] bg-[#603EE4] font-semibold rounded-[8px]">
             Get Started
           </button>
         </div>
       </header>
+
+      {/* Mobile Header */}
+      <header className="md:hidden fixed flex w-full justify-between items-center p-4 z-50">
+        <div className="flex w-full">
+          <Image src={logo} width={160} height={80} alt="Logo" />
+        </div>
+
+        <div className="mil-menu-btn cursor-pointer z-50" onClick={toggleMenu}>
+          <span className="block w-6 h-0.5 bg-white mb-1"></span>
+          <span className="block w-6 h-0.5 bg-white mb-1"></span>
+          <span className="block w-6 h-0.5 bg-white"></span>
+        </div>
+      </header>
+
+      {/* Mobile Menu */}
+      <div
+        className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 backdrop-filter backdrop-blur-md transition-all duration-500  transform z-50 ${
+          isOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <div className="flex justify-between items-center px-4 py-4">
+          <Image src={logo} width={160} height={80} alt="Logo" />
+          <button
+            className="text-white text-4xl font-thin"
+            onClick={toggleMenu}
+          >
+            &times;
+          </button>
+        </div>
+
+        <nav className="flex flex-col items-center justify-center h-full space-y-6 pb-[140px]">
+          {navLinks.map((link, index) => (
+            <Link
+              key={index}
+              href={link.href}
+              className="font-bold text-white text-[24px]"
+              onClick={toggleMenu}
+            >
+              {link.name}
+            </Link>
+          ))}
+          <div>
+            <button className="text-[#FFFFFF] leading-[27.2px] text-[16px] py-2 px-[16px] bg-[#603EE4] font-semibold rounded-[8px]">
+              Get Started
+            </button>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -4,84 +4,54 @@ import HeroImage1 from "../../Assets/images/heroImage1.png";
 import HeroImage2 from "../../Assets/images/heroImage2.png";
 import HeroImage3 from "../../Assets/images/heroImage3.png";
 import HeroVector from "../../Assets/vector/heroVector.png";
+import Arrow from "../../Assets/svg/arrow.svg";
 const Hero: React.FC = () => {
   return (
-    <section className="flex items-center  h-screen flex justify-center  relative ">
+    <section className="flex items-center  h-screen flex justify-center  relative px-4 sm:pt-0 pt-[100px]">
       <div className="absolute top-0 right-0">
         <Image src={HeroVector} width={216} height={335} alt="Logo" />
       </div>
       <div className="max-w-[1224px] w-full ">
-        <div className=" flex flex-row  w-full ">
+        <div className=" flex xl:flex-row flex-col xl:gap-0 gap-[20px]  w-full ">
           <div className=" flex-1 text-white w-full">
-            <div className="flex flex-col gap-[20px]">
-              <h1 className="bg-[#19171E] max-w-max py-2 px-3 rounded-full border-[1px] border-[#FFFFFF33]">
+            <div className="flex flex-col xl:gap-[20px] gap-[12px] xl:items-start items-center">
+              <h1 className="bg-[#19171E] max-w-max py-2 px-3 rounded-full border-[1px] md:text-[16px] text-[12px] border-[#FFFFFF33]">
                 🏆 First AI powered project management tool
               </h1>
               <div>
-                <h1 className="text-[56px] leading-[64.4px] tracking-[-2px] font-medium">
-                  From Chaos to Control{" "}
+                <h1 className="xl:text-[56px] md:text-[46px] text-[32px]  xl:leading-[64.4px] lg:leading-[56px] leading-[46px] tracking-[-2px] font-medium flex  flex-col xl:items-start items-center ">
+                  From Chaos to Control
                   <span className="text-[#C18EFF]"> Master Your Tasks </span>
                 </h1>
-                <h1 className="text-[56px] leading-[64.4px] tracking-[-2px] font-medium flex items-center gap-3">
+                <h1 className="xl:text-[56px] md:text-[46px] text-[32px] xl:leading-[64.4px] md:leading-[56px] text-[36px] tracking-[-2px] font-medium flex xl:justify-start justify-center md:gap-3 gap-2 items-center">
                   with
                   <span>
-                    <svg
-                      width="51"
-                      height="51"
-                      viewBox="0 0 51 51"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g clip-path="url(#clip0_1_14)">
-                        <g clip-path="url(#clip1_1_14)">
-                          <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
-                            d="M25.03 45.8C36.0758 45.8 45.03 36.8458 45.03 25.8C45.03 14.7542 36.0758 5.79999 25.03 5.79999C13.9842 5.79999 5.03 14.7542 5.03 25.8C5.03 36.8458 13.9842 45.8 25.03 45.8ZM34.2975 24.0325L26.7975 16.5325C26.5669 16.2937 26.291 16.1033 25.986 15.9722C25.681 15.8412 25.353 15.7722 25.021 15.7694C24.6891 15.7665 24.3599 15.8297 24.0526 15.9554C23.7454 16.0811 23.4662 16.2668 23.2315 16.5015C22.9968 16.7362 22.8111 17.0154 22.6854 17.3226C22.5597 17.6298 22.4965 17.959 22.4994 18.291C22.5023 18.6229 22.5712 18.951 22.7022 19.256C22.8333 19.561 23.0237 19.8369 23.2625 20.0675L26.4942 23.3H17.53C16.867 23.3 16.2311 23.5634 15.7622 24.0322C15.2934 24.5011 15.03 25.1369 15.03 25.8C15.03 26.463 15.2934 27.0989 15.7622 27.5678C16.2311 28.0366 16.867 28.3 17.53 28.3H26.4942L23.2625 31.5325C23.0237 31.7631 22.8333 32.039 22.7022 32.344C22.5712 32.649 22.5023 32.977 22.4994 33.309C22.4965 33.6409 22.5597 33.9701 22.6854 34.2774C22.8111 34.5846 22.9968 34.8637 23.2315 35.0985C23.4662 35.3332 23.7454 35.5188 24.0526 35.6445C24.3599 35.7702 24.6891 35.8335 25.021 35.8306C25.353 35.8277 25.681 35.7588 25.986 35.6277C26.291 35.4967 26.5669 35.3063 26.7975 35.0675L34.2975 27.5675C34.7662 27.0987 35.0295 26.4629 35.0295 25.8C35.0295 25.1371 34.7662 24.5013 34.2975 24.0325Z"
-                            fill="#B880FC"
-                          />
-                        </g>
-                      </g>
-                      <defs>
-                        <clipPath id="clip0_1_14">
-                          <rect
-                            x="0.0299988"
-                            y="0.799988"
-                            width="50"
-                            height="50"
-                            rx="25"
-                            fill="white"
-                          />
-                        </clipPath>
-                        <clipPath id="clip1_1_14">
-                          <rect
-                            width="50"
-                            height="50"
-                            fill="white"
-                            transform="translate(0.0299988 0.799988)"
-                          />
-                        </clipPath>
-                      </defs>
-                    </svg>
-                  </span>{" "}
+                    <Image
+                      src={Arrow}
+                      width={50}
+                      height={50}
+                      alt="Logo"
+                      className="md:w-[50px] w-[32px]"
+                    />
+                  </span>
                   Axento
                 </h1>
               </div>
               <div>
-                <h2 className="font-normal text-[18px] leading-[23px]">
+                <h2 className="font-normal xl:text-[18px] md:text-[16px]  text-[12px] leading-[23px] xl:text-start text-center">
                   Sign up for a free trial today and experience the difference.
                   Our <br /> intuitive interface, powerful features, and
                   dedicated.
                 </h2>
               </div>
               <div>
-                <button className="text-[#FFFFFF] leading-[27.2px]  text-[16px] py-[10px] px-[35px] bg-[#603EE4] font-medium rounded-[58px] ">
+                <button className="text-[#FFFFFF] leading-[27.2px]  md:text-[16px] text-[12px] md:py-[10px] py-1 md:px-[35px] px-4 bg-[#603EE4] font-medium rounded-[58px] ">
                   Start 14 Days Free Trial
                 </button>
               </div>
             </div>
           </div>
-          <div className=" flex-1 text-white w-full flex flex-row gap-[14px]  ">
+          <div className=" flex-1 text-white w-full flex flex-row gap-[14px]  justify-center">
             <div className="flex flex-col gap-[10px]">
               <Image src={HeroImage1} width={216} height={335} alt="Logo" />
               <Image src={HeroImage3} width={147} height={51} alt="Logo" />
