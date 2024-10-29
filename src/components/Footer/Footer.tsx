583126764b91285a77ef7febe8cc1cd092e46a67
Mathumitha Gnanasekaran <102413599+Mathumitha-Gnanasekaran@users.noreply.github.com>
2024-10-29 17:37:29 +0530

    update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
-import HeroImage1 from '../../Assets/images/heroImage1.png'
-import HeroImage2 from '../../Assets/images/heroImage2.png'
-import Gabriel from '../../Assets/images/Gabriel.png'
-import CreateVector from '../../Assets/svg/CreateVector.svg'
-import DesignVector from '../../Assets/svg/DesignVector.svg'
-import DesktopDesign from '../../Assets/svg/DesktopDesign.svg'
-import FidelityVector from '../../Assets/svg/FidelityVector.svg'
-import PurpleVector from '../../Assets/svg/PurpleVector.svg'
-import Tick from '../../Assets/svg/Tick.svg'
-
-
+import logo from "../../Assets/Logo/logo.png";
+import HeroImage1 from "../../Assets/images/heroImage1.png";
+import HeroImage2 from "../../Assets/images/heroImage2.png";
+import Gabriel from "../../Assets/images/Gabriel.png";
+import CreateVector from "../../Assets/svg/CreateVector.svg";
+import DesignVector from "../../Assets/svg/DesignVector.svg";
+import DesktopDesign from "../../Assets/svg/DesktopDesign.svg";
+import FidelityVector from "../../Assets/svg/FidelityVector.svg";
+import PurpleVector from "../../Assets/svg/PurpleVector.svg";
+import Tick from "../../Assets/svg/Tick.svg";
+import HiPurVector from "../../Assets/svg/HiPurVector.svg";
+import HiBorVector from "../../Assets/svg/HiBorVerctor.svg";
+import Facebook from "../../Assets/Logo/Facebook.svg";
+import Twitter from "../../Assets/Logo/Twitter.svg";
+import Instagram from "../../Assets/Logo/Instagram.svg";
+import Linkedin from "../../Assets/Logo/Linkedin.svg";
 
 export default function Footer() {
   return (
-    <div className="bg-[#19171E] text-[#fff] font-sans flex flex-col items-center justify-center py-16 mt-32">
-      <section className="w-full max-w-5xl p-8 bg-[#0E0C12] rounded-3xl shadow-xl flex flex-col lg:flex-row items-center gap-10 mb-16 relative overflow-hidden border-[1px] border-[#FFFFFF33]">
-        <div className="absolute top-0 right-0 blur-[100px] rounded-full bg-[#b880fc] w-[500px] h-[300px] opacity-[0.6]"></div>
+    <div className="bg-[#19171E] text-[#fff] font-sans flex flex-col items-center justify-center py-10 mt-32">
+      <section className="w-full max-w-[1200px] mx-auto relative rounded-3xl h-[445px] bg-[#0E0C12] shadow-xl flex flex-col lg:flex-row items-center gap-10 mb-16 overflow-hidden border-[1px] border-[#FFFFFF33]">
+        <div className="absolute top-0 right-0 blur-[100px] rounded-full bg-[#b880fc] w-[500px] h-[320px] opacity-[0.6]"></div>
 
-        <div className="flex-1 space-y-6 relative z-10 left-5 ">
-          <h1 className="text-[55.34px] tracking-[-1px] leading-[64.4px]">
+        <div className="flex-1 space-y-6 relative z-10 left-20">
+          <h1 className="text-[55.34px] tracking-[-1px] leading-[64.4px] w-[74%]">
             Take Control of{" "}
             <span className="text-[#B880FC]">Your Tasks Today.</span>
           </h1>
-          <p className="text-[18px] leading-[30.6px]">
+          <p className="text-[18px] leading-[30.6px] w-[74%]">
             Unlock the full potential of efficient task management with axento.
             Sign up for our free trial today and experience the difference.
           </p>
@@ -33,67 +38,112 @@ export default function Footer() {
         </div>
 
         <div className="flex-1 flex justify-center items-center">
-        <div className="absolute text-[#fff] w-full">
-              <div className="absolute w-[17%] bottom-[-150px] right-[530px]">
-                <Image src={HeroImage1} width={216} height={335} alt="Image" />
-              </div>
-              <div className="absolute w-[17%] bottom-[60px] right-[350px] opacity-[0.99]">
+          <div className="absolute text-[#fff] w-full">
+            <div className="absolute w-[15%] bottom-[-185px] right-[585px]">
+              <Image src={HeroImage1} width={216} height={335} alt="Image" />
+            </div>
+            <div className="absolute w-[12%] bottom-[60px] right-[395px] opacity-[0.99]">
               <Image src={Gabriel} alt="Image" />
-
-              </div>
-              <div className="absolute w-[21%] h-[235px] bg-[#19171E] rounded-[20px] top-[-47.5px] right-[252px]">
-                <div className="absolute ">
+            </div>
+            <div className="absolute w-[21%] h-[255px] bg-[#19171E] rounded-[21px] top-[-35.5px] right-[311.5px] overflow-hidden">
+              <div className="absolute w-[70%] top-12 left-10">
                 <Image src={DesktopDesign} alt="Image" />
+              </div>
+
+              <div className="relative w-[92%] h-[61px] left-10 top-32 z-10">
+                <div className="absolute inset-0 rounded-[10px] bg-gradient-to-r from-[#E26EE51A] to-[#A076F91A]"></div>
+
+                <div className="absolute top-[2.5px] left-[2px] w-[calc(100%_-_4px)] h-[calc(100%_-_5px)] rounded-[9px] bg-gradient-to-r from-[#A076F9] to-[#A076F900] opacity-40"></div>
+
+                <div className="relative flex items-center h-full px-4">
+                  <div className="relative flex items-center justify-center w-8 h-8 mr-3">
+                    <Image
+                      src={PurpleVector}
+                      alt="Icon"
+                      className="w-full h-full"
+                    />
+                    <Image src={Tick} alt="Tick" className="absolute w-4 h-4" />
+                  </div>
+
+                  <div className="space-y-2">
+                    <Image src={CreateVector} alt="Create Wireframe" />
+                    <Image src={DesignVector} alt="#design" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="relative w-[92%] h-[61px] left-10 top-36 z-10">
+                <div className="absolute inset-0 rounded-[10px] bg-gradient-to-r from-[#E26EE51A] to-[#A076F91A]"></div>
+
+                <div className="absolute top-[1.8px] left-[2px] w-[calc(100%_-_3px)] h-[calc(100%_-_4px)] rounded-[9px] bg-[#19171E]"></div>
+
+                <div className="relative flex items-center h-full px-4">
+                  <div className="relative flex items-center justify-center w-8 h-8 mr-3">
+                    <Image
+                      src={HiBorVector}
+                      alt="Icon"
+                      className="w-full h-full"
+                    />
+                    <Image
+                      src={HiPurVector}
+                      alt="Tick"
+                      className="absolute w-8 h-8"
+                    />
+                  </div>
+
+                  <div className="space-y-2">
+                    <Image src={FidelityVector} alt="HI-Fidelity" />
+                    <Image src={DesignVector} alt="#design" />
+                  </div>
                 </div>
               </div>
             </div>
-            </div>
+          </div>
+        </div>
       </section>
 
-      {/* Footer Links Section */}
-      <footer className="w-full max-w-5xl flex flex-col md:flex-row justify-between text-gray-300 text-sm border-t border-gray-800 pt-8">
+      <footer className="w-full max-w-5xl flex flex-col md:flex-row justify-between text-gray-300 text-sm pt-8">
         <div className="space-y-4 mb-8 md:mb-0">
-          <h2 className="text-2xl font-semibold text-purple-400">axento.</h2>
-          <p>Sign up for a free trial today and experience the difference.</p>
+        <div className="flex items-center">
+          <Image src={logo} width={200} height={80} alt="Logo" />
+        </div>
+          <p>Sign up for a free trial today and 
+          experience the difference.</p>
           <div className="flex space-x-4">
             <Link href="https://facebook.com">
               <Image
-                src="/assets/logo/Facebook.svg"
-                alt="Facebook"
-                width={24}
-                height={24}
+                src={Facebook} alt="Logo"
+                width={14}
+                height={14}
               />
             </Link>
             <Link href="https://twitter.com">
               <Image
-                src="/assets/logo/Twitter.svg"
-                alt="Twitter"
-                width={24}
-                height={24}
+                src={Twitter} alt="Logo"
+                width={14}
+                height={14}
               />
             </Link>
             <Link href="https://instagram.com">
               <Image
-                src="/assets/logo/Instagram.svg"
-                alt="Instagram"
-                width={24}
-                height={24}
+                src={Instagram} alt="Logo"
+                width={14}
+                height={14}
               />
             </Link>
             <Link href="https://linkedin.com">
               <Image
-                src="/assets/logo/Linkedin.svg"
-                alt="LinkedIn"
-                width={24}
-                height={24}
+                src={Linkedin} alt="Logo"
+                width={14}
+                height={14}
               />
             </Link>
           </div>
         </div>
         <div className="grid grid-cols-3 gap-10">
           <div>
-            <h3 className="text-lg font-semibold text-gray-200">Features</h3>
-            <ul className="space-y-2">
+            <h3 className="text-[16px] leading-[27.2px] font-urbanist text-[#B880FC]">Features</h3>
+            <ul className="space-y-3 text-[#FCFDF2] mt-7">
               <li>Project Management</li>
               <li>Gamification</li>
               <li>Collaboration</li>
@@ -101,10 +151,10 @@ export default function Footer() {
             </ul>
           </div>
           <div>
-            <h3 className="text-lg font-semibold text-gray-200">
+            <h3 className="text-[16px] leading-[27.2px] font-urbanist text-[#B880FC]">
               Integrations
             </h3>
-            <ul className="space-y-2">
+            <ul className="space-y-3 text-[#FCFDF2] mt-7">
               <li>Notion</li>
               <li>Monday</li>
               <li>Clickup</li>
@@ -112,8 +162,8 @@ export default function Footer() {
             </ul>
           </div>
           <div>
-            <h3 className="text-lg font-semibold text-gray-200">Resources</h3>
-            <ul className="space-y-2">
+            <h3 className="text-[16px] leading-[27.2px] font-urbanist text-[#B880FC]">Resources</h3>
+            <ul className="space-y-3 text-[#FCFDF2] mt-7">
               <li>Brand Guideline</li>
               <li>Newsroom</li>
             </ul>
--- src/pages/ClientReviews/ClientReviews.tsx
+++ src/pages/ClientReviews/ClientReviews.tsx
@@ -9,10 +9,12 @@ import ReadMore from "../../Assets/svg/readMore.svg";
 import Scroll1 from "../../Assets/svg/Button - Scroll to page 1.svg";
 import Scroll2 from "../../Assets/svg/Button - Scroll to page 2.svg";
 import Scroll3 from "../../Assets/svg/Button - Scroll to page 3.svg";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ClientReviews = () => {
   return (
-    <div className="relative w-full h-[774.6px] overflow-hidden text-white font-inter">
+    <div className="relative flex flex-col w-full overflow-hidden text-white font-inter">
       <div className="w-full flex flex-col justify-center items-center text-[#fff] mt-32">
         <div className="bg-[#19171E] py-2 px-5 rounded-full border-[1px] border-[#FFFFFF33] mb-6">
           <h1 className="md:text-[16px] text-[12px] font-inter text-ivory">
@@ -28,96 +30,139 @@ const ClientReviews = () => {
         <div className="text-[48px] leading-[67.2px] font-medium">Axento</div>
       </div>
 
-      <div className="absolute top-1/2 w-[1470px] h-[557px] overflow-hidden text-gray-300">
-        <div className="flex space-x-8">
-          <div className="bg-gray-300 rounded-2xl p-4 w-[350px]">
-            <p className="text-gray-400 mb-4">
-              The reporting and analytics features are invaluable for making
-              strategic decisions. A fantastic investment for any organization.
-            </p>
-            <div className="flex items-center space-x-4">
+      <div className="p-8 relative overflow-visible pb-10">
+        <Carousel
+          showStatus={false}
+          showArrows={false}
+          centerSlidePercentage={50}
+          centerMode={true}
+          renderIndicator={(clickHandler, isSelected, index, label) => (
+            <button
+              type="button"
+              onClick={clickHandler}
+              className={`w-3 h-3 rounded-full mx-1.5 ${
+                isSelected ? "bg-[#613FE7]" : "bg-[#9b88e6]"
+              }`}
+              aria-label={`${label} ${index + 1}`}
+            />
+          )}
+          className="h-[350px]"
+        >
+          <div className="rounded-2xl p-6 w-[98%] h-[300px] flex space-x-6 border-gray-800 border-[0.5px]">
+            <div className="w-1/3 h-full border-r-[0.5px] border-gray-800 pr-5">
               <Image
-                src="/4ySpgr3hgnnvdrlIcGCBt9X6eI.jpg.png"
+                src={Inaya}
                 alt="Alien Donald"
-                width={100}
-                height={100}
-                className="rounded-full"
+                className="rounded h-full object-cover"
               />
-              <div>
-                <p className="text-white font-medium">Alien Donald</p>
-                <p className="text-gray-500">Product Manager</p>
+            </div>
+            <div className="w-2/3 h-full flex flex-col justify-center">
+              <p className="text-gray-400 mb-4 text-left">
+                The reporting and analytics features are invaluable for making
+                strategic decisions. A fantastic investment for any
+                organization.
+              </p>
+              <div className="flex items-center space-x-4  pt-6">
+                <div>
+                  <Image
+                    src={Donald}
+                    alt="Alien Donald"
+                    className="rounded-full h-10 w-10 object-cover"
+                  />
+                </div>
+                <div className="flex flex-col items-start">
+                  <p className="text-white font-medium">Alien Donald</p>
+                  <p className="text-gray-500">Product Manager</p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="bg-gray-300 rounded-2xl p-4 w-[350px]">
-            <p className="text-gray-400 mb-4">
-              Being a small business owner, time is of the essence. Axento has
-              been a lifesaver in helping us organize projects and communicate
-              effectively.
-            </p>
-            <div className="flex items-center space-x-4">
+          <div className="rounded-2xl p-6 w-[98%] h-[300px] flex space-x-6 border-gray-800 border-[0.5px]">
+            <div className="w-1/3 h-full border-r-[0.5px] border-gray-800 pr-5">
               <Image
-                src="/QVVUsx3EcaYbJDdtzYN9Cxz6SE.png.png"
-                alt="Isabela Inaya"
-                width={100}
-                height={100}
-                className="rounded-full"
+                src={Inaya}
+                alt="Alien Donald"
+                className="rounded h-full object-cover"
               />
-              <div>
-                <p className="text-white font-medium">Isabela Inaya</p>
-                <p className="text-gray-500">Product Manager</p>
+            </div>
+            <div className="w-2/3 h-full flex flex-col justify-center">
+              <p className="text-gray-400 mb-4 text-left">
+                Being a small business owner, time is of the essence. Axento has
+                been a lifesaver in helping us organize projects and communicate
+                effectively.
+              </p>
+              <div className="flex items-center space-x-4 pt-6">
+                <div>
+                  <Image
+                    src={Inaya2}
+                    alt="Alien Donald"
+                    className="rounded-full h-10 w-10 object-cover"
+                  />
+                </div>
+                <div className="flex flex-col items-start">
+                  <p className="text-white font-medium">Isabela Inaya</p>
+                  <p className="text-gray-500">Product Manager</p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="bg-gray-300 rounded-2xl p-4 w-[350px]">
-            <p className="text-gray-400 mb-4">
-              When potential customers are researching you online, they're
-              getting to know you through the content on your website.
-            </p>
-            <div className="flex items-center space-x-4">
+          <div className="rounded-2xl p-6 w-[98%] h-[300px] flex space-x-6 border-gray-800 border-[0.5px]">
+            <div className="w-1/3 h-full border-r-[0.5px] border-gray-800 pr-5">
               <Image
-                src="/fJHNKouWD1BIaJb4hU6hKytbXQ.png.png"
-                alt="Customer Image"
-                width={100}
-                height={100}
-                className="rounded-full"
+                src={Client3}
+                alt="Alien Donald"
+                className="rounded h-full object-cover"
               />
-              <div>
-                <p className="text-white font-medium">Customer Name</p>
-                <p className="text-gray-500">Customer Position</p>
+            </div>
+            <div className="w-2/3 h-full flex flex-col justify-center">
+              <p className="text-gray-400 mb-4 text-left">
+                B When potential customers are researching you online, they're
+                getting to know you through the content on your website.
+              </p>
+              <div className="flex items-center space-x-4 pt-6">
+                <div>
+                  <Image
+                    src={Inaya2}
+                    alt="Alien Donald"
+                    className="rounded-full h-10 w-10 object-cover"
+                  />
+                </div>
+                <div className="flex flex-col items-start">
+                  <p className="text-white font-medium">Customer Name</p>
+                  <p className="text-gray-500">Customer Position</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Carousel>
       </div>
-
-      <div className="w-full relative rounded-[50px] h-[30px] overflow-hidden">
+      {/* <div className="w-full relative rounded-[50px] h-[30px] overflow-hidden">
         <div className="absolute top-[calc(50%_-_15px)] left-[calc(50%_-_35px)] w-[25px] h-[30px] overflow-hidden">
           <img
-            src={Scroll1} 
+            src={Scroll1}
             alt="Image"
             className="absolute top-[calc(50%_-_5px)] left-[calc(50%_-_12.5px)] w-2.5 h-2.5 rounded-full"
           />
         </div>
         <div className="absolute top-[calc(50%_-_15px)] left-[calc(50%_-_10px)] w-5 h-[30px] overflow-hidden">
           <img
-            src={Scroll2} 
+            src={Scroll2}
             alt="Image"
             className="absolute top-[calc(50%_-_5px)] left-[calc(50%_-_5px)] w-2.5 h-2.5 rounded-full opacity-50"
           />
         </div>
         <div className="absolute top-[calc(50%_-_15px)] left-[calc(50%_+_10px)] w-[25px] h-[30px] overflow-hidden">
           <img
-            src={Scroll3} 
+            src={Scroll3}
             alt="Image"
             className="absolute top-[calc(50%_-_5px)] left-[calc(50%_-_12.5px)] w-2.5 h-2.5 rounded-full opacity-50"
           />
         </div>
-      </div>
+      </div> */}
 
-      <div className="absolute bottom-12 left-1/2 transform -translate-x-1/2 text-center">
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-center">
         <div className="flex items-center gap-x-2">
           <p className="font-medium">Read our customer stories</p>
           <Image
